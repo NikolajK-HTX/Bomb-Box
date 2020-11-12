de49c6839bba31fcfbe36ef9026b8591510d2f08
Emil Hejlesen <emil@hejlesen.eu>
2020-11-12 14:44:45 +0100

    lavede api

---
+++ APIvejr.py
@@ -0,0 +1,15 @@
+
+import requests
+
+
+
+def getWeather():
+    key = "c59712f4440365ed016958173d3b05cf"
+    CITY = "Aarhus"
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    URL = BASE_URL + "q=" + CITY + "&appid=" + key
+    response = requests.get(URL)
+    data = response.json()
+    temperature = round(data['main']['temp']-273.15)
+    return temperature
+
