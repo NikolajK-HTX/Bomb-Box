7dece50596fb4fc4da5fc976a8a9dc61020f6324
Nikolaj J-K <kagenogmig@gmail.com>
2020-11-16 09:13:05 +0100

    Update APIvejr.py

--- APIvejr.py
+++ APIvejr.py
@@ -1,15 +1,20 @@
-
-import requests
-
+import requests, datetime
 
 
-def getWeather():
-    key = "c59712f4440365ed016958173d3b05cf"
-    CITY = "Aarhus"
-    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-    URL = BASE_URL + "q=" + CITY + "&appid=" + key
-    response = requests.get(URL)
-    data = response.json()
-    temperature = round(data['main']['temp']-273.15)
-    return temperature
+class weatherAPI:
+    lastTimeUpdated = datetime.datetime.now()
 
+    def getWeather(self):
+        key = "c59712f4440365ed016958173d3b05cf"
+        CITY = "Aarhus"
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        URL = BASE_URL + "q=" + CITY + "&appid=" + key
+        response = requests.get(URL)
+        data = response.json()
+        temperature = round(data['main']['temp']-273.15)
+        return temperature
+
+    def timeSinceLastUpdate(self):
+        now = datetime.datetime.now()
+        differenceInSeconds = (now - weatherAPI().lastTimeUpdated).total_seconds()
+        return differenceInSeconds
