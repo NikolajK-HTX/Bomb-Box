656bd7f4de7ce15b97f2d2b6a1c5ed17d7299555
Nikolaj J-K <kagenogmig@gmail.com>
2020-11-16 09:54:14 +0100

    Bruger APIvejr til at hente temperaturen
    Opdaterer nu temperaturen hvert andet minut.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from APIvejr import weatherAPI
-import os, datetime
-if os.name != "nt": import grovepi
+import os
+import datetime
+if os.name != "nt": # importeres kun hvis programmet ikke køres på windows
+    import grovepi
 
 
+vejr = weatherAPI()
+temperatur = vejr.getTemperature()
+
+while True:
+    # opdater temperaturen hvert andet minut
+    if vejr.timeSinceLastUpdate() > 120:
+        temperatur = vejr.getTemperature()
+        print("Temperaturen er {} grader.".format(temperatur))
