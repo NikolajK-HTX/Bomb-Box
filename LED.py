3dbf815a5b2defa9096afc661aac696b8876d141
Nikolaj J-K <kagenogmig@gmail.com>
2020-11-26 13:57:52 +0100

    Use grovepi

--- LED.py
+++ LED.py
@@ -1,5 +1,5 @@
 import time
-#from grovepi import *
+from grovepi import *
 import random
 import datetime
 
@@ -7,17 +7,17 @@ import datetime
 class LED():
     def __init__(self, pin):
         self.led = pin
-        # pinMode(self.led, "OUTPUT")
+        pinMode(self.led, "OUTPUT")
         self.timer = Timer(0.5)
         self.blinkState = 0
 
     def on(self, type):
         if type == 0:
             try:
-                # digitalWrite(self.led, 1)  # Send HIGH to switch on LED
+                digitalWrite(self.led, 1)  # Send HIGH to switch on LED
                 print("LED ON!")
             except KeyboardInterrupt:  # Turn LED off before stopping
-                # digitalWrite(self.led, 0)
+                digitalWrite(self.led, 0)
                 print()
             except IOError:  # Print "Error" if communication error encountered
                 print("Error")
@@ -27,16 +27,16 @@ class LED():
                 if self.timer.isDone():
                     self.timer.start()
                     if self.blinkState:
-                        # digitalWrite(self.led, 1)  # Send HIGH to switch on LED
-                        print("LED ON!")
+                        digitalWrite(self.led, 1)  # Send HIGH to switch on LED
+                        # print("LED ON!")
                     if not self.blinkState:
-                        # digitalWrite(self.led, 0)  # Send LOW to switch off LED
-                        print("LED OFF!")
+                        digitalWrite(self.led, 0)  # Send LOW to switch off LED
+                        # print("LED OFF!")
                     self.blinkState = not self.blinkState
 
             except KeyboardInterrupt:  # Turn LED off before stopping
-                # digitalWrite(self.led, 0)
-                print()
+                digitalWrite(self.led, 0)
+                # print()
             except IOError:  # Print "Error" if communication error encountered
                 print("Error")
 
--- button.py
+++ button.py
@@ -1,14 +1,14 @@
-# import grovepi
+import grovepi
 
 
 class button():
     def __init__(self, pin):
         self.button = pin
-        # grovepi.pinMode(button, "INPUT")
+        grovepi.pinMode(self.button, "INPUT")
 
     def buttonCheck(self):
         try:
-            # self.buttonState = grovepi.digitalRead(button)
-            print()
+            self.buttonState = grovepi.digitalRead(self.button)
+            # print()
         except IOError:
             print("Error")
