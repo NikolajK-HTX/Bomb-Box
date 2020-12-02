d3f5c4b1daf3efa5b2825d5cb5b4b5f607cdcc13
Nikolaj J-K <kagenogmig@gmail.com>
2020-12-02 12:39:31 +0100

    Create buzzer.py

---
+++ buzzer.py
@@ -0,0 +1,26 @@
+import grovepi
+import time
+
+class BuzzerCountdown:
+    def __init__(self, pin, TotalMinutes):
+        self.pin = pin
+        grovepi.pinMode(self.pin, "OUTPUT")
+        self.Total = TotalMinutes
+        self.buzz = False
+        self.count = 0
+    
+    def on(self):
+        if (time.time() >= (time.time() + self.count * 5)) and not self.buzz:
+            self.buzz = True
+            grovepi.digitalWrite(self.pin, 1)
+        elif (time.time() - (time.time() + self.count * 5) >= 0.2) and self.buzz:
+            self.buzz = False
+            grovepi.digitalWrite(self.pin, 0)
+
+        
+
+        
+        
+
+
+
