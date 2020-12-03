107389273c65303beca3bcdac433c4f5d76cb977
Nikolaj J-K <kagenogmig@gmail.com>
2020-12-03 15:09:35 +0100

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -11,6 +11,16 @@ class BuzzerCountdown:
         self.isOn = False
     
     def on(self):
+        if self.totalOnTime - time.time() <= 30 and self.interval != 2:
+            self.interval = 2
+        elif self.totalOnTime - time.time() <= 10 and self.interval != 1:
+            self.interval = 1
+        elif self.totalOnTime - time.time() <= 5 and self.interval != 0.4:
+            self.interval = 0.4
+        elif self.totalOnTime - time.time() <= 3 and self.interval != 0.25:
+            self.interval = 0.25
+
+
         if time.time() >= self.lastBuzz + self.interval and not self.isOn:
             grovepi.digitalWrite(self.pin, 1)
             self.isOn = True
@@ -18,15 +28,6 @@ class BuzzerCountdown:
             grovepi.digitalWrite(self.pin, 0)
             self.lastBuzz = time.time()
             self.isOn = False
-        
-        if self.totalOnTime - time.time() <= 30 and self.interval != 2:
-            self.interval = 2
-        if self.totalOnTime - time.time() <= 10 and self.interval != 1:
-            self.interval = 1
-        if self.totalOnTime - time.time() <= 5 and self.interval != 0.4:
-            self.interval = 0.4
-        if self.totalOnTime - time.time() <= 3 and self.interval != 0.25:
-            self.interval = 0.25
 
     def off(self):
         grovepi.digitalWrite(self.pin, 0)
