cb777f966f1677107bc202c8cb57cfac247de5e3
Nikolaj J-K <kagenogmig@gmail.com>
2020-12-07 10:06:44 +0100

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -21,8 +21,8 @@ class BuzzerCountdown:
             self.isOn = False
             # opdater tid interval
             buzzTimer = self.totalOnTime - time.time()
-            if buzzTimer <= 9.12:
-                buzzTimer = 9.12
+            if buzzTimer <= 8.91:
+                buzzTimer = 8.91
             self.interval = math.log10(buzzTimer)*5 - 4.5
             print("Timer: {} og Buzz: {}".format(buzzTimer, self.interval))
 
