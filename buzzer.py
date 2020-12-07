92db81858aa6cf670e2c16733a2b451b1d96c5e2
Nikolaj J-K <kagenogmig@gmail.com>
2020-12-07 09:03:23 +0100

    buzzer follow log function

--- buzzer.py
+++ buzzer.py
@@ -1,5 +1,6 @@
 import grovepi
 import time
+import math
 
 class BuzzerCountdown:
     def __init__(self, pin, totalTimeOn, interval):
@@ -11,16 +12,6 @@ class BuzzerCountdown:
         self.isOn = False
     
     def on(self):
-        if self.totalOnTime - time.time() <= 30 and self.interval != 2:
-            self.interval = 2
-        elif self.totalOnTime - time.time() <= 10 and self.interval != 1:
-            self.interval = 1
-        elif self.totalOnTime - time.time() <= 5 and self.interval != 0.4:
-            self.interval = 0.4
-        elif self.totalOnTime - time.time() <= 3 and self.interval != 0.25:
-            self.interval = 0.25
-
-
         if time.time() >= self.lastBuzz + self.interval and not self.isOn:
             grovepi.digitalWrite(self.pin, 1)
             self.isOn = True
@@ -28,6 +19,8 @@ class BuzzerCountdown:
             grovepi.digitalWrite(self.pin, 0)
             self.lastBuzz = time.time()
             self.isOn = False
+            self.interval = abs(3*math.log10(self.totalOnTime - time.time()))
+
 
     def off(self):
         grovepi.digitalWrite(self.pin, 0)
